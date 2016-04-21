177b4ebf5058c80db4836fb839cac7253f54c624
Liang Xingchen <liang@maichong.it>
2016-04-21 19:52:06 +0800

    0.8.0

--- src/view.jsx
+++ src/view.jsx
@@ -10,10 +10,19 @@ import { shallowEqual } from 'alaska-admin-view';
 import DateTime from 'react-datetime';
 import 'react-datetime/css/react-datetime.css';
 import moment from 'moment';
-import 'moment/locale/zh-tw';
 import 'moment/locale/zh-cn';
 
 export default class DateFieldView extends React.Component {
+
+  static propTypes = {
+    model: React.PropTypes.object,
+    field: React.PropTypes.object,
+    data: React.PropTypes.object,
+    errorText: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    value: React.PropTypes.any,
+    onChange: React.PropTypes.func,
+  };
 
   static contextTypes = {
     settings: React.PropTypes.object
@@ -28,29 +37,51 @@ export default class DateFieldView extends React.Component {
   }
 
   render() {
-    let props = this.props;
-    let value = props.value;
-    let field = props.field;
-    let errorText = props.errorText;
+    let { value, field, disabled, errorText, onChange } = this.props;
     let help = field.help;
     let className = 'form-group';
     if (errorText) {
       className += ' has-error';
       help = errorText;
     }
+    if (field.format && value) {
+      value = moment(value).format(field.format);
+    }
     let helpElement = help ? <p className="help-block">{help}</p> : null;
+    let inputElement;
+    if (field.static) {
+      inputElement = <p className="form-control-static">{value}</p>;
+    } else if (disabled) {
+      inputElement = <input type="text" className="form-control" disabled value={value}/>;
+    } else {
+      inputElement = <DateTime
+        value={value}
+        dateFormat={field.format}
+        timeFormat={false}
+        onChange={onChange}
+      />;
+    }
+
+    let label = field.nolabel ? '' : field.label;
+
+    if (field.fullWidth) {
+      let labelElement = label ? (
+        <label className="control-label">{label}</label>
+      ) : null;
+      return (
+        <div className={className}>
+          {labelElement}
+          {inputElement}
+          {helpElement}
+        </div>
+      );
+    }
+
     return (
       <div className={className}>
-        <label className="col-sm-2 control-label">{field.label}</label>
+        <label className="col-sm-2 control-label">{label}</label>
         <div className="col-sm-10">
-          {
-            disabled ? <input type="text" className="form-control" disabled value={value}/> : <DateTime
-              value={value}
-              dateFormat={field.format}
-              timeFormat={false}
-              onChange={props.onChange}
-            />
-          }
+          {inputElement}
           {helpElement}
         </div>
       </div>
