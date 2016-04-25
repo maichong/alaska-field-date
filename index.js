9fa4e6e2f562fc435a95625e5624b133f463694b
Liang Xingchen <liang@maichong.it>
2016-04-25 22:34:41 +0800

    0.9.0

--- index.js
+++ index.js
@@ -13,15 +13,16 @@ class DateField extends alaska.Field {
   init() {
     let field = this;
     field.format = field.format || 'YYYY-MM-DD';
+    if (!field.filter && field.filter !== false) {
+      field.filter = 'DatetimeFieldFilter';
+    }
     this.underscoreMethod('format', function (format) {
       return moment(this.get(field.path)).format(format || field.format);
     });
   }
 
   createFilter(filter) {
-    if (!filter) {
-      return;
-    }
+    if (!filter) return;
     let value;
 
     //精确
@@ -46,6 +47,8 @@ class DateField extends alaska.Field {
       bt = filter;
     } else if (filter.$bt && filter.$bt instanceof Array) {
       bt = filter.$bt;
+    } else if (filter.bt && filter.bt instanceof Array) {
+      bt = filter.bt;
     }
     if (bt && bt.length === 2) {
       let start = moment(bt[0]);
@@ -59,14 +62,12 @@ class DateField extends alaska.Field {
     }
 
     //比较
-    ['$gt', '$gte', '$lt', '$lte'].forEach((key) => {
-      let val = filter[key];
+    ['gt', 'gte', 'lt', 'lte'].forEach((key) => {
+      let val = filter[key] || filter['$' + key];
       if (val) {
         if (!(val instanceof Date)) {
           val = moment(val);
-          if (!val.isValid()) {
-            return;
-          }
+          if (!val.isValid()) return;
           if (key[1] === 'g') {
             //$gt $gte
             val = val.startOf('day').toDate();
@@ -78,7 +79,7 @@ class DateField extends alaska.Field {
         if (!value) {
           value = {};
         }
-        value[key] = val;
+        value['$' + key] = val;
       }
     });
     if (value) {
